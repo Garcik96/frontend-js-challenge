2be6c03211632cb7da843f5484762f5589818d0b
Iván García Ballesteros <garcia.ballesteros96@gmail.com>
2023-09-07 08:15:14 +0200

    refactor trend list design

--- src/app/trends/trends-list/trends-list.component.ts
+++ src/app/trends/trends-list/trends-list.component.ts
@@ -8,17 +8,28 @@ import { selectTrendsByProvider } from '../store/selectors';
 @Component({
   selector: 'app-trends-list',
   template: `
-    <article class="trend" *ngFor="let trend of trends$ | async">
-      <a class="trend__link" routerLink="/trends/{{ trend.id }}">
-        <figure class="trend__figure">
-          <img class="trend__image" [src]="trend.image" [alt]="trend.title" />
-          <figcaption class="trend__title">
-            <h2>{{ trend.title }}</h2>
-          </figcaption>
-        </figure>
-        <p class="trend__excerpt">{{ trend.body[0] }}</p>
-      </a>
-    </article>
+    <section class="trend__container">
+      <article class="trend" *ngFor="let trend of trends$ | async">
+        <a class="trend__link" routerLink="/trends/{{ trend.id }}">
+          <figure class="trend__figure">
+            <img
+              *ngIf="trend.image"
+              class="trend__image"
+              [src]="trend.image"
+              [alt]="trend.title" />
+            <img
+              *ngIf="!trend.image"
+              class="trend__image"
+              src="assets/Iconos/Favicon/favicon_avantio.svg"
+              [alt]="trend.title" />
+            <figcaption class="trend__title">
+              <h2>{{ trend.title }}</h2>
+            </figcaption>
+          </figure>
+          <p class="trend__excerpt">{{ trend.body[0] }}</p>
+        </a>
+      </article>
+    </section>
     <button
       class="app-button app-button--primary app-button--floating"
       (click)="openCreateTrendSidebar()">
